0dda8c9cdbbeec3972f567e854891f5f0a01f567
leejiho <gd1115@naver.com>
2020-05-26 14:07:58 +0900

    탭이미지

--- app/src/main/java/com/example/coookbab/MainActivity.java
+++ app/src/main/java/com/example/coookbab/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TabHost;
 
 
@@ -21,22 +23,37 @@ public class MainActivity extends TabActivity {
         TabHost tabHost = getTabHost();
         TabHost.TabSpec spec;
 
+        ImageView tabwidget01 = new ImageView(this);
+        tabwidget01.setImageResource(R.drawable.tab_01);
+        ImageView tabwidget02 = new ImageView(this);
+        tabwidget02.setImageResource(R.drawable.tab_02);
+        ImageView tabwidget03 = new ImageView(this);
+        tabwidget03.setImageResource(R.drawable.tab_03);
+
         Intent intent = new Intent().setClass(this, RecipeMain.class);
-        spec = tabHost.newTabSpec("recipe").setIndicator("레시피").setContent(intent);
+        spec = tabHost.newTabSpec("recipe").setIndicator(tabwidget01).setContent(intent);
         tabHost.addTab(spec);
 
         //두번째 탭
         intent = new Intent().setClass(this, RefrigeratorMain.class);
-        spec = tabHost.newTabSpec("refri").setIndicator("냉장고").setContent(intent);
+        spec = tabHost.newTabSpec("refri").setIndicator(tabwidget02).setContent(intent);
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(0);
 
         //세번째 탭
         intent = new Intent().setClass(this, MyRecipeMain.class);
-        spec = tabHost.newTabSpec("my").setIndicator("내 레시피").setContent(intent);
+        spec = tabHost.newTabSpec("my").setIndicator(tabwidget03).setContent(intent);
         tabHost.addTab(spec);
 
         tabHost.setCurrentTab(0);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int screenHeight = metrics.heightPixels;
+
+        tabHost.getTabWidget().getChildAt(0).getLayoutParams().height = (screenHeight * 15) / 200;
+        tabHost.getTabWidget().getChildAt(1).getLayoutParams().height = (screenHeight * 15) / 200;
+        tabHost.getTabWidget().getChildAt(2).getLayoutParams().height = (screenHeight * 15) / 200;
     }
 }
